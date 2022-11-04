df4f18f7cc7187e9f8b45d807cb1baf9183dbd03
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-11-04 21:52:52 +0800

    feat: Add Home page grid layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { FC } from 'react'
 import styles from './Home.module.sass'
 
-const Home = () => {
-  return <div>Home</div>
+const Home: FC = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.sidebar}></div>
+      <div className={styles.main}></div>
+    </div>
+  )
 }
 
 export default Home
