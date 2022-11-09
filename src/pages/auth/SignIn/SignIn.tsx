58e0c71203af217936121ee0086a1e7ea14f593d
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-11-09 19:26:41 +0800

    feat: Add SignIn form validation

--- src/pages/auth/SignIn/SignIn.tsx
+++ src/pages/auth/SignIn/SignIn.tsx
@@ -2,14 +2,43 @@ import React, { FC } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTableList } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import * as yup from 'yup'
 import styles from '../auth.module.sass'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Input } from '../../../components/ui/Input'
 
 export interface SignInFormInput {
   email: string
   password: string
 }
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .required('Email address is required')
+    .email('Invalid Email address'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(8, 'Password length should be at least 8 characters')
+    .max(20, 'Password cannot exceed more than 20 characters'),
+})
+
 const SignIn: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<SignInFormInput>({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit: SubmitHandler<SignInFormInput> = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className={styles.root}>
       <div className={styles.card}>
@@ -20,18 +49,21 @@ const SignIn: FC = () => {
           <span className={styles.text}>Todo App</span>
         </div>
         <div className={styles.title}>Sign in</div>
-        <form>
-          <div className={styles['text-field']}>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="your@email.com" />
-            <span className={styles.error}>Invalid email</span>
-          </div>
-          <div className={styles['text-field']}>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="your password" />
-          </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            label="Email address"
+            name="email"
+            register={register}
+            error={errors.email?.message}
+          />
+          <Input
+            label="Password"
+            name="password"
+            register={register}
+            error={errors.password?.message}
+          />
           <div className={styles.button}>
-            <button type="submit" disabled>
+            <button type="submit" disabled={!isValid}>
               Sign in
             </button>
           </div>
