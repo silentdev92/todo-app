9d45c6af7f9d2612999babd4dc3c4432945dafb0
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-11-12 21:52:15 +0800

    feat: Add Home page ui

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import { faCalendar, faCalendarDays } from '@fortawesome/free-solid-svg-icons'
 import { SidebarItem } from '../../components/SidebarItem'
 import styles from './Home.module.sass'
@@ -7,6 +8,8 @@ import { TodoItem } from '../../components/TodoItem'
 import { TodoForm } from '../../components/TodoForm'
 
 const Home: FC = () => {
+  const [addTodoFormIsOpen, setAddTodoFormIsOpen] = useState<boolean>(false)
+
   return (
     <div className={styles.root}>
       <div className={styles.sidebar}>
@@ -25,8 +28,26 @@ const Home: FC = () => {
         />
       </div>
       <div className={styles.main}>
-        <TodoItem />
-        <TodoForm />
+        <div className={styles.title}>Today</div>
+        <div className={styles.completed}>4/6 completed</div>
+        <div className={styles.list}>
+          <TodoItem />
+        </div>
+        {addTodoFormIsOpen ? (
+          <TodoForm />
+        ) : (
+          <div
+            className={styles['add-button']}
+            onClick={() => {
+              setAddTodoFormIsOpen(true)
+            }}
+          >
+            <div className={styles.icon}>
+              <FontAwesomeIcon icon={faCirclePlus} />
+            </div>
+            <div className={styles.text}>Add todo</div>
+          </div>
+        )}
       </div>
     </div>
   )
