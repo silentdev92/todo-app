bb3e956a693a18c33ea99c3e0349b293be6d30c1
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-11-29 17:39:19 +0800

    feat: Add transitions to todo form

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { CSSTransition } from 'react-transition-group'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import { faCalendar, faCalendarDays } from '@fortawesome/free-solid-svg-icons'
 import { SidebarItem } from '../../components/SidebarItem'
@@ -12,9 +13,29 @@ import TodoService from '../../api/TodoService'
 import { setTodos } from '../../store/todo/slice'
 import { TodoList } from '../../components/TodoList'
 import { selectTodoList } from '../../store/todo/selectors'
+import { delay } from '../../helpers/delay'
 
 const Home: FC = () => {
   const [addTodoFormIsOpen, setAddTodoFormIsOpen] = useState<boolean>(false)
+
+  const [showButton, setShowButton] = useState<boolean>(true)
+  const [showForm, setshowForm] = useState<boolean>(false)
+
+  const changeDisplay = async () => {
+    if (addTodoFormIsOpen) {
+      setShowButton(false)
+      await delay(300)
+      setshowForm(true)
+    } else {
+      setshowForm(false)
+      await delay(300)
+      setShowButton(true)
+    }
+  }
+
+  useEffect(() => {
+    changeDisplay()
+  }, [addTodoFormIsOpen])
 
   const list = useAppSelector(selectTodoList)
 
@@ -57,9 +78,42 @@ const Home: FC = () => {
         <div className={styles.list}>
           <TodoList list={list} />
         </div>
-        {addTodoFormIsOpen ? (
+        <CSSTransition
+          addEndListener={(node: HTMLElement, done: () => void) => {
+            node.addEventListener('transitionend', done, false)
+          }}
+          in={showForm}
+          timeout={300}
+          classNames={{
+            enter: styles['slide-enter'],
+            enterActive: styles['slide-enter-active'],
+            enterDone: styles['slide-enter-done'],
+            exit: styles['slide-exit'],
+            exitActive: styles['slide-exit-active'],
+            exitDone: styles['slide-exit-done'],
+          }}
+          mountOnEnter
+          unmountOnExit
+        >
           <TodoForm onClose={() => setAddTodoFormIsOpen(false)} />
-        ) : (
+        </CSSTransition>
+        <CSSTransition
+          addEndListener={(node: HTMLElement, done: () => void) => {
+            node.addEventListener('transitionend', done, false)
+          }}
+          in={showButton}
+          timeout={300}
+          classNames={{
+            enter: styles['fade-enter'],
+            enterActive: styles['fade-enter-active'],
+            enterDone: styles['fade-enter-done'],
+            exit: styles['fade-exit'],
+            exitActive: styles['fade-exit-active'],
+            exitDone: styles['fade-exit-done'],
+          }}
+          mountOnEnter
+          unmountOnExit
+        >
           <div
             className={styles['add-button']}
             onClick={() => {
@@ -71,7 +125,7 @@ const Home: FC = () => {
             </div>
             <div className={styles.text}>Add todo</div>
           </div>
-        )}
+        </CSSTransition>
       </div>
     </div>
   )
