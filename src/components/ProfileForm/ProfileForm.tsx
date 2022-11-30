264805aacc705bc25c8dd2c4d7fda29f9e1bc3bf
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-11-30 17:52:06 +0800

    feat: Add validation to profile & settings forms

--- src/components/ProfileForm/ProfileForm.tsx
+++ src/components/ProfileForm/ProfileForm.tsx
@@ -1,23 +1,84 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Button } from '../ui/Button'
 import { Input } from '../ui/Input'
 import styles from './ProfileForm.module.sass'
 
+export interface FormInput {
+  email: string
+  firstName: string
+  lastName: string
+}
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .required('Email address is required')
+    .email('Invalid Email address'),
+  firstName: yup
+    .string()
+    .required('First name is required')
+    .min(2, 'First name length should be at least 2 characters')
+    .max(15, 'First name cannot exceed more than 15 characters'),
+  lastName: yup
+    .string()
+    .required('Last name is required')
+    .min(2, 'Last name length should be at least 2 characters')
+    .max(15, 'Last name cannot exceed more than 15 characters'),
+})
+
 const ProfileForm = () => {
-  const { register } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<FormInput>({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit: SubmitHandler<FormInput> = async ({
+    email,
+    firstName,
+    lastName,
+  }) => {
+    console.log(email, firstName, lastName)
+  }
 
   return (
-    <form className={styles.root}>
-      <Input label="First name" name="firstName" register={register} />
-      <Input label="Last name" name="lastName" register={register} />
-      <Input label="Email address" name="email" register={register} />
+    <form className={styles.root} onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        label="First name"
+        name="firstName"
+        register={register}
+        error={errors.firstName?.message}
+      />
+      <Input
+        label="Last name"
+        name="lastName"
+        register={register}
+        error={errors.lastName?.message}
+      />
+      <Input
+        label="Email address"
+        name="email"
+        register={register}
+        error={errors.email?.message}
+      />
       <div className={styles.actions}>
         <div className={styles.button}>
-          <Button text="Apply" />
+          <Button
+            text="Apply"
+            type="submit"
+            variant="contained"
+            disabled={!isValid}
+          />
         </div>
         <div className={styles.button}>
-          <Button text="Cancel" variant="outlined" />
+          <Button text="Cancel" variant="outlined" onClick={() => reset()} />
         </div>
       </div>
     </form>
--- src/components/SettingsForm/SettingsForm.tsx
+++ src/components/SettingsForm/SettingsForm.tsx
@@ -1,25 +1,73 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Button } from '../ui/Button'
 import { Input } from '../ui/Input'
 import styles from './SettingsForm.module.sass'
 
+export interface FormInput {
+  password: string
+  passwordConfirm: string
+}
+
+const schema = yup.object().shape({
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(8, 'Password length should be at least 8 characters')
+    .max(20, 'Password cannot exceed more than 20 characters'),
+  passwordConfirm: yup
+    .string()
+    .required('Confirm password is required')
+    .min(8, 'Password length should be at least 8 characters')
+    .max(20, 'Password cannot exceed more than 20 characters')
+    .oneOf([yup.ref('password')], 'Passwords do not match'),
+})
+
 const SettingsForm = () => {
-  const { register } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<FormInput>({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit: SubmitHandler<FormInput> = async ({
+    password,
+    passwordConfirm,
+  }) => {
+    console.log(password, passwordConfirm)
+  }
+
   return (
-    <form className={styles.root}>
-      <Input label="Password" name="password" register={register} />
+    <form className={styles.root} onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        label="Password"
+        name="password"
+        register={register}
+        error={errors.password?.message}
+      />
       <Input
         label="Confirm password"
         name="passwordConfirm"
         register={register}
+        error={errors.passwordConfirm?.message}
       />
       <div className={styles.actions}>
         <div className={styles.button}>
-          <Button text="Apply" />
+          <Button
+            text="Apply"
+            type="submit"
+            variant="contained"
+            disabled={!isValid}
+          />
         </div>
         <div className={styles.button}>
-          <Button text="Cancel" variant="outlined" />
+          <Button text="Cancel" variant="outlined" onClick={() => reset()} />
         </div>
       </div>
     </form>
